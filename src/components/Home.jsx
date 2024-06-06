b00e2bc2237f0307c7d48e5eec941f944d13d257
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-06-06 21:37:28 +0530

    did some changes in styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,31 +7,32 @@ import Loading from "./Loading";
 function Home() {
   const [products, setProduct] = useContext(ProductContext);
 
-
   return (
     <>
-     {!products ? <Loading/> : <> 
-      <Nav />
-      <div className="w-[85%] p-10 pt-[5%] flex flex-wrap gap-6 overflow-x-hidden overflow-y-auto">
-
-        {products.map((product) => (
-          <Link key={product.id}
-            to={`/details/${product.id}`}
-            className="card p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center"
-          >
-            <div
-              className="hover:scale-110 mb-5 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-              style={{
-                backgroundImage:
-                  `url(${product.image})`,
-              }}
-            ></div>
-            <h1 className="hover:text-red-400">{product.title}</h1>
-          </Link>
-        ))}
-
-      </div>
-     </>}
+      {!products ? (
+        <Loading />
+      ) : (
+        <>
+          <Nav />
+          <div className="w-[85%] p-10 pt-[5%] flex flex-wrap gap-6 overflow-x-hidden overflow-y-auto">
+            {products.map((product) => (
+              <Link
+                key={product.id}
+                to={`/details/${product.id}`}
+                className="card p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center"
+              >
+                <div
+                  className="hover:scale-110 mb-5 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+                  style={{
+                    backgroundImage: `url(${product.image})`,
+                  }}
+                ></div>
+                <h1 className="hover:text-red-400">{product.title}</h1>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
     </>
   );
 }
