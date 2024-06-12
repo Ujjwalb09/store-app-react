692a4993d371f3138eee04fa982828e6403b7783
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-06-12 13:22:46 +0530

    implemented go back button on detials page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -8,12 +8,34 @@ function Details() {
 
   const [products] = useContext(ProductContext);
 
-  console.log(products);
+  const product = products
+    ? products.find((product) => product.id == id)
+    : null;
 
-  const product = products ? products.find((product) => product.id == id) : null;
-
-  return !product ? <Loading/> : (
-    <div className="w-[70%] flex h-full justify-between items-center m-auto p-[10%]">
+  return !product ? (
+    <Loading />
+  ) : (
+    <div className="w-[70%] flex h-full justify-between items-center m-auto p-[10%] relative">
+      <Link
+        to="/"
+        className="absolute left-1 top-20 w-full flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-700 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+      >
+        <svg
+          className="w-5 h-5 rtl:rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+          />
+        </svg>
+        <span>Go back</span>
+      </Link>
       
       <img
         className="object-contain h-[80%]  w-[40%]"
