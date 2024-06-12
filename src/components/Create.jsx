aba813f9695db4d3483dd8a0e586de951c2965ab
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-06-12 18:06:11 +0530

    completed the form in create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
@@ -7,8 +8,45 @@ const Create = () => {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
 
+  const addProduct = (e) => {
+    e.preventDefault();
+    const product = {
+      title,
+      image,
+      category,
+      price,
+      description,
+    };
+  };
+
   return (
-    <form className="flex flex-col items-center p-[5%] w-screen h-screen">
+    <form
+      onSubmit={(e) => {
+        addProduct(e);
+      }}
+      className="flex flex-col items-center p-[5%] w-screen h-screen"
+    >
+      <Link
+        to="/"
+        className="absolute left-40 top-12 w-full flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-700 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700"
+      >
+        <svg
+          className="w-5 h-5 rtl:rotate-180"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+          />
+        </svg>
+        <span>Go back</span>
+      </Link>
+
       <h1 className="mb-5 w-1/2 text-3xl">Add new Product</h1>
       <input
         type="url"
